5fdfa90f7025b6be2a1363262bec7bf3351aad24
ressayb <112770731+ressayb@users.noreply.github.com>
2022-12-05 05:06:24 +0100

    Update hackit.py

--- hackit.py
+++ hackit.py
@@ -6,7 +6,7 @@ from hiphp import *
 p1=hiphp(key="123",url="http://127.0.0.1/index.php")#Default: retu=False.
 
 # Get the hole Code:
-p1.get_hole()
+#p1.get_hole()
 
 # Command line interface:
 p1.cli()
